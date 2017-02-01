39cb929d6d8bb20ffa8aeb7376aad3a0f202badd
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2017-02-01 14:19:15 +0100

    Add game data on the home page

--- tictactoe/models.py
+++ tictactoe/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 # Create your models here.
 GAME_STATUS_CHOICES = (
@@ -10,6 +11,13 @@ GAME_STATUS_CHOICES = (
 )
 
 
+class GamesManager(models.Manager):
+    def games_for_user(self, user):
+        """Return a queryset of games that this user participates in"""
+        return super(GamesManager, self).get_queryset().filter(
+            Q(first_player_id=user.id) | Q(second_player_id=user.id))
+
+
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="games_first_player")
     second_player = models.ForeignKey(User, related_name="games_second_player")
@@ -17,6 +25,8 @@ class Game(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='A', choices=GAME_STATUS_CHOICES)
+
+    objects = GamesManager()
 
     def __str__(self):
         return "{0} vs {1}".format(self.first_player, self.second_player)
