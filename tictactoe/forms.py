8fa68802f7d6c29d13e30e0944a836a5724ef447
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2017-03-02 21:17:43 +0100

    Add field options

--- tictactoe/forms.py
+++ tictactoe/forms.py
@@ -6,3 +6,5 @@ class InvitationForm(forms.ModelForm):
     class Meta:
         model = Invitation
         fields = '__all__'
+
+        exclude = ['from_user']
--- tictactoe/views.py
+++ tictactoe/views.py
@@ -8,7 +8,9 @@ from .forms import InvitationForm
 @login_required
 def new_invitation(request):
     if request.method == 'POST':
-        form = InvitationForm(data=request.POST)
+        # Se inicializa una nueva entidad para completar el usuario que ahora no aparece en la vista
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(data=request.POST, instance=invitation)
         if form.is_valid():
             form.save()
             return redirect('user_home')
